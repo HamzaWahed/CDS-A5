36dc3c4dadfea1c85061e345b26128cdb3eb12ca
hamza wahed <hzd454@gmail.com>
2024-11-03 11:18:04 -0400

    add clang-format and update gitignore

---
+++ src/lf.cpp
@@ -0,0 +1,7 @@
+#include "deserialize.hpp"
+#include <sdsl/int_vector.hpp>
+#include <sdsl/rank_support.hpp>
+#include <string>
+#include <vector>
+
+int main(int argc, char **argv) {}
---
+++ src/lf_movelet.cpp
@@ -0,0 +1,6 @@
+#include <sdsl/int_vector.hpp>
+#include <sdsl/rank_support.hpp>
+#include <string>
+#include <vector>
+
+
